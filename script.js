d85d506de979f69825a0d882fb086ca0c8fce93b
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-19 02:29:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function indexOfIgnoreCase(s1, s2) {
-    return s1.toLowerCase().indexOf(s2.toLowerCase());
+    return s1.toLowerCase().indexOf(s2.toLowerCase()) !== -1 ? s1.toLowerCase().indexOf(s2.toLowerCase()) : -1;
 }
-
-Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
